ec11f21ff9474e2228e5c268cc0b8bf0c36b9d36
trishanguyen <trishan.2018@gmail.com>
2016-10-20 16:11:26 -0700

    Update AutoBlue.java
    Compared auto red to auto blue and altered the powers and negative signs to correspond with the previous code
    Unsure about code starting from case 10
    There are some questions throughout the code that have been commented out

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/AutoBlue.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/AutoBlue.java
@@ -16,17 +16,20 @@ import com.qualcomm.robotcore.hardware.DcMotorController;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.UltrasonicSensor;
 
-public class AutoBlue extends BotHardware {
+public class AutoBlue extends BotHardware 
+{
     private short state = 0;
     private short close, far, good;
 
     @Override
-    public void runOpMode() throws InterruptedException {
+    public void runOpMode() throws InterruptedException 
+    {
         super.runOpMode();
         close = far = good = 0;
         initHardware();
 
-        while (gyro.isCalibrating()) {
+        while (gyro.isCalibrating()) 
+        {
             Thread.sleep(50);
         }
 
@@ -36,7 +39,8 @@ public class AutoBlue extends BotHardware {
 
         setTime();
 
-        while (opModeIsActive()) {
+        while (opModeIsActive()) 
+        {
             telemetry.addData("State", state);
             telemetry.addData("Z", gyro.getIntegratedZValue());
             telemetry.addData("Beacon Red", beacon.red());
@@ -45,76 +49,94 @@ public class AutoBlue extends BotHardware {
             telemetry.addData("Time: ", getTime());
             telemetry.addData("Color:", groundLeft.blue());
 
-            switch (state) {
+            switch (state) 
+            {
                 case 0://start drive
                     setPower(0.25f);
                     state++;
                     break;
-
-                case 1:
-                    if(getTime() > 1.4){
+                case 1: //after 1 sec stop to turn
+                    if(getTime() > 1.4) // auto red had only 1 sec; should this be 1.4 or 1???
+                    {
                         setPower(0);
                         state++;
                     }
                     break;
-                case 2://first turn
+                case 2: //first turn 45 degrees //CHANGE (auto red was opp for the negatives)
                     setPower(-0.35f, 0.35f);
-                    if(Math.abs(gyro.getIntegratedZValue()) > 44){
+                    if(Math.abs(gyro.getIntegratedZValue()) > 44)
+                    {
                         setPower(0);
                         state++;
                     }
                     break;
-                case 3:
+                case 3: //reset gyro
                     gyro.resetZAxisIntegrator();
                     state++;
                     break;
-                case 4://drive until white line
+                case 4: //drive until white line
                     driveGyro(0.3f);
-                    if(getTime() > 10){
+                    if(getTime() > 10)
+                    {
                         setPower(0);
                         state = 100;
                         break;
                     }
-                    if(isLeftOnLine()){
+                    if(isLeftOnLine())
+                    {
                         state ++;
                         setTime();
                     }
                     break;
-                case 5:
-                    if(getTime() > 0.2) {
+                case 5: //turn
+                    if(getTime() > 0.2) 
+                    {
                         gyro.resetZAxisIntegrator();
-                        setPower(-0.35f, 0.35f);
+                        setPower(-0.35f, 0.35f); //auto red is opposite negative signs
                         state++;
                     }
                     break;
                 case 6: //getting into proper position on white line
-                    if(Math.abs(gyro.getIntegratedZValue()) > 55){
-                        setPower(0.35f, -0.35f);
-                    } else if(Math.abs(gyro.getIntegratedZValue()) > 44){
+                    if(Math.abs(gyro.getIntegratedZValue()) > 55)
+                    {
+                        setPower(0.35f, -0.35f); //auto red had opp negative signs
+                    } 
+                    else if(Math.abs(gyro.getIntegratedZValue()) > 44)
+                    {
                         setPower(0f);
                         gyro.resetZAxisIntegrator();
                         state++;
                     }
                     break;
-                case 7: //drive forward for good position for dumping climbers
-                    if(sonar.getUltrasonicLevel() < 21) {
+                case 7: //drive forward until good distance for measuring color of beacon
+                    if(sonar.getUltrasonicLevel() < 21) 
+                    {
                         far = good = 0;
                         close++;
-                    } else if (sonar.getUltrasonicLevel() > 24) {
+                    } 
+                    else if (sonar.getUltrasonicLevel() > 24) 
+                    {
                         close = good = 0;
                         far++;
-                    } else {
+                    } 
+                    else 
+                    {
                         close = far = 0;
                         good++;
                     }
 
-                    if (close == 2) {
+                    if (close == 2) 
+                    {
                         setPower(-0.1f);
                         close = 0;
-                    } else if (far == 2) {
+                    } 
+                    else if (far == 2) 
+                    {
                         setPower(0.1f);
                         far = 0;
-                    } else if (good == 2) {
+                    } 
+                    else if (good == 2) 
+                    {
                         setPower(0);
                         thrower.setPosition(1);
                         state++;
@@ -122,74 +144,119 @@ public class AutoBlue extends BotHardware {
                     }
                     break;
                 case 8: //beacon pusher
-                    try {
+                    try 
+                    {
                         Thread.sleep(2500);
-                    }catch (InterruptedException e){
+                    }
+                    catch (InterruptedException e)
+                    {
                         telemetry.addData("ERROR", e.getStackTrace()[0]);
                     }
-                    if(beacon.red() < 5 && beacon.blue() < 5){
-                        thrower.setPosition(0);
+                    if(beacon.red() < 5 && beacon.blue() < 5)
+                    {
                         beaconServo.setPosition(0.1);
                         telemetry.addData("Beacon", "FAILED");
                         state = 100;
                         break;
                     }
-                    if(beacon.red() > beacon.blue()){
+                    if(beacon.red() > beacon.blue())
+                    {
                         telemetry.addData("RED color: ", beacon.red());
                         telemetry.addData("BLUE color: ", beacon.blue());
-                        thrower.setPosition(0);
-                        try {
-                            Thread.sleep(2000);
-                        }catch (InterruptedException e){
-                            telemetry.addData("ERROR", e.getStackTrace()[0]);
-                        }
-                        beaconServo.setPosition(0);
-                    } else {
-                        telemetry.addData("BLUE color: ", beacon.blue());
-                        telemetry.addData("RED color: ", beacon.red());
-                        thrower.setPosition(0);
                         try {
                             Thread.sleep(2000);
                         }catch (InterruptedException e){
                             telemetry.addData("ERROR", e.getStackTrace()[0]);
                         }
                         beaconServo.setPosition(1);
+                    } 
+                    else 
+                    {
+                        telemetry.addData("BLUE color: ", beacon.blue());
+                        telemetry.addData("RED color: ", beacon.red());
+                        try 
+                        {
+                            Thread.sleep(2000);
+                        }
+                        catch (InterruptedException e)
+                        {
+                            telemetry.addData("ERROR", e.getStackTrace()[0]);
+                        }
+                        beaconServo.setPosition(1); //changed to 1, auto red is zero
                     }
-                    try {
+                    try 
+                    {
                         Thread.sleep(1000);
-                    }catch (InterruptedException e){
+                    }
+                    catch (InterruptedException e)
+                    {
                         telemetry.addData("ERROR", e.getStackTrace()[0]);
                     }
                     state++;
                     setPower(0.35f);
                     break;
-
                 case 9: //drive forward until push beacon
-                    try {
+                    try 
+                    {
                         Thread.sleep(1250);
-                    }catch (InterruptedException e){
+                    }
+                    catch (InterruptedException e)
+                    {
                         telemetry.addData("ERROR", e.getStackTrace()[0]);
                     }
                     setPower(0);
                     state++;
                     break;
-                /*
-                case 10:
+                
+                
+                //Unshare about further edits from this point
+                case 10: //rotate 90 degrees
                      gyro.resetZAxisIntegrator();
                      setPower(0.35f, -0.35f);
-                      if(Math.abs(gyro.getIntegratedZValue()) < 89){
+                     if(Math.abs(gyro.getIntegratedZValue()) < 89)
+                     {
                         setPower(0);
-                        try {
-                            Thread.sleep(1000);
-                        }catch (InterruptedException e){
-                            telemetry.addData("ERROR", e.getStackTrace()[0]);
-                        }
-                        setPower(0.35f);
-                        Thread.sleep(2500);
+                        state++;
+                     }
+                     break;
+               case 11: //drive forward until white line
+                    gyro.resetZAxisIntegrator();
+                    driveGyro(0.3f);
+                    if(getTime() > 10)
+                    {
                         setPower(0);
+                        state = 100;
+                        break;
                     }
-                 */
-                default:
+                    if(isLeftOnLine())
+                    {
+                        state++;
+                        setTime();
+                    }
+                    break;
+               case 12: //rotate 90 degrees to face beacon again -- check direction of turn
+                     gyro.resetZAxisIntegrator();
+                     setPower(0.35f, -0.35f);
+                     if(Math.abs(gyro.getIntegratedZValue()) < 89)
+                     {
+                        setPower(0);
+                        state++;
+                     }
+                     break;
+               case 13: //push beacon -- check code
+                    try 
+                    {
+                        Thread.sleep(1250);
+                    }
+                    catch (InterruptedException e)
+                    {
+                        telemetry.addData("ERROR", e.getStackTrace()[0]);
+                    }
+                    setPower(0);
+                    state++;
+                    break;
+               case 14: //drive backwards to center
+               default:
                     setPower(0);
                     break;
             }
